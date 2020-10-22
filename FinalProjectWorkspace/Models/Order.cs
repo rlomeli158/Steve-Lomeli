f88ed460247e974b7ebd213383617b855d342f18
Steve Lomeli <rlomeli158@gmail.com>
2020-10-21 20:46:45 -0500

    Updated models with Order class
    Updated with model code for Order class, very rough draft

--- FinalProjectWorkspace/Models/Order.cs
+++ FinalProjectWorkspace/Models/Order.cs
@@ -9,11 +9,32 @@ namespace FinalProjectWorkspace.Models
 
     public class Order
     {
+        private const Decimal SALES_TAX = 0.0825m;
+
         public Int32 OrderID { get; set; }
+
+        public Int32 TransactionNumber { get; set; } //Will we need this?
+
         public DateTime OrderDate { get; set; }
-        //public Customer Customer { get; set; }
+
+        public Decimal OrderSubtotal
+        {
+            get { return Tickets.Sum(t => t.TotalCost); }
+        }
+
+        public Decimal Taxes
+        {
+            get { return OrderSubtotal * SALES_TAX; }
+        }
+
+        public Decimal OrderTotal
+        {
+            get { return OrderSubtotal + Taxes; }
+        }
 
         public List<Ticket> Tickets { get; set; }
+
+        //public Customer Customer { get; set; } or public AppUser User {get;set;} ?
 
         public Order()
         {
