1af7204bd27994ea75ffa85c72193228fc3bc8df
Jeremiah Nathaniel <jeremiahw124@gmail.com>
2020-10-30 13:01:12 -0500

    added AppUser
    Added AppUser

--- FinalProjectWorkspace/Models/AppUser.cs
+++ FinalProjectWorkspace/Models/AppUser.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using Microsoft.AspNetCore.Identity;
 
@@ -6,19 +7,60 @@ namespace FinalProjectWorkspace.Models
 {
     public class AppUser : IdentityUser
     {
-        [Display(Name = "First Name")]
-        [Required(ErrorMessage = "First name is required.")]
+        //User ID is inherited
+
+        [Display(Name = "Enter user first name")]
+        [Required(ErrorMessage ="Please enter your first name!")]
         public String FirstName { get; set; }
 
-        [Display(Name = "Last Name")]
-        [Required(ErrorMessage = "Last name is required.")]
+        [Display(Name = "Enter user a middle initial")]
+        [Required(ErrorMessage = "Please enter your middle initial!")]
+        public String MiddleInitial { get; set; }
+
+        [Display(Name = "Enter user last name")]
+        [Required(ErrorMessage = "Please enter your last name!")]
         public String LastName { get; set; }
 
-        /* In case we need full name
-        public String FullName
-        {
-            get { return FirstName + " " + LastName; }
-        }
-        */
+        //phone numebr is inherited
+
+        [Display(Name = "Enter user address")]
+        [Required(ErrorMessage = "Please enter your address!")]
+        public String Address { get; set; }
+
+        [Display(Name = "Enter user city")]
+        [Required(ErrorMessage = "Please enter your city!")]
+        public String City { get; set; }
+
+        [Display(Name = "Enter user state")]
+        [Required(ErrorMessage = "Please enter your state!")]
+        public String State { get; set; }
+
+        [Display(Name = "Enter user Zip")]
+        [Required(ErrorMessage = "Please enter your Zip!")]
+        public String Zip { get; set; }
+
+        [Display(Name = "Enter user birthday")]
+        [Required(ErrorMessage = "Please enter your birthday!")]
+        public DateTime Birthday { get; set; }
+
+        //do not store password since it is inherited
+
+        [Display(Name = "Popcorn Point Balance")]
+        public Int32 PCPBalance { get; set; }
+
+        [Display(Name = "Enter user SSN")]
+        [Required(ErrorMessage = "Please enter your SSN!")]
+        public String SSN { get; set; }
+
+        [Display(Name = "Enter user employee type")]
+        [Required(ErrorMessage = "Please select the employee type!")]
+        public String EmpType { get; set; }
+
+        public List<Order> Orders { get; set; }
+        public List<MovieReview> MovieReviews { get; set; }
+        public List<Ticket> Tickets { get; set; }
+
+
     }
+    
 }
