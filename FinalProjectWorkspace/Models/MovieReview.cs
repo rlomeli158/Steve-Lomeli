9b5c935cf23ead23b22f52c2fabab81550d9e35e
jennifer1957 <70177102+jennifer1957@users.noreply.github.com>
2020-11-13 02:06:29 -0600

    Started movie reviews

--- FinalProjectWorkspace/Models/MovieReview.cs
+++ FinalProjectWorkspace/Models/MovieReview.cs
@@ -9,9 +9,12 @@ namespace FinalProjectWorkspace.Models
     public class MovieReview
     {
         public Int32 MovieReviewID { get; set; }
+        [Display(Name = "Review Date")]
+        [DisplayFormat(DataFormatString = "{0:MM/dd/yyyy}")]
         public DateTime ReviewDate { get; set; }
         public Boolean ApprovalStatus { get; set; }
         public Int32 Rating { get; set; }
+        [Display(Name = "Review Description")]
         public String ReviewDescription { get; set; }
 
         public Movie Movie { get; set; }
