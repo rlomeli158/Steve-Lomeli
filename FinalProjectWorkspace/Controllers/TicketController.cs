a9813d757d9a2d33eedf46dbd2eb2af7e9cc4057
Steve Lomeli <rlomeli158@gmail.com>
2020-12-02 18:10:40 -0600

    ticket status cancelled from reports

--- FinalProjectWorkspace/Controllers/TicketController.cs
+++ FinalProjectWorkspace/Controllers/TicketController.cs
@@ -763,7 +763,7 @@ namespace FinalProjectWorkspace.Controllers
         {
             //Initial query LINQ
             var query = from m in _context.Ticket select m;
-            query = query.Include(m => m.Showing).ThenInclude(m => m.Movie).Include(m => m.Order).ThenInclude(m => m.Purchaser);
+            query = query.Include(m => m.Showing).ThenInclude(m => m.Movie).Include(m => m.Order).ThenInclude(m => m.Purchaser).Where(t => t.TicketStatus != "Cancelled");
 
             //If statements corresponding to each input form control
 
